5c899bfd4ab881d20f469dfb7ced443c895386a7
Hiann Manso <hiannmanso@gmail.com>
2024-09-04 22:18:44 -0300

    fix: infos

--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,11 +1,12 @@
 import { Header } from "../../components/Header"
+import WeddingCountdown from "../../components/WeddingCountdown"
 // import WeddingCountdown from "../../components/WeddingCountdown"
 
 export function Home() {
 	return (
 		<>
         <Header/>
-
+		<WeddingCountdown weddingDate="2024-10-26T00:00:00" />
 		{/* <WeddingCountdown weddingDate="2024-10-26T00:00:00" /> */}
 		</>
 	)
